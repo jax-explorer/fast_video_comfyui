4b74aa00cc86086cf7712d53bac95da1de2e36fd
jax <jax_builder@gamil.com>
2024-04-30 13:02:10 +0800

    change name

--- __init__.py
+++ __init__.py
@@ -4,5 +4,5 @@ import sys
 modules_path = os.path.join(os.path.dirname(__file__), "fast")
 from .fast.utils import *
 NODE_CLASS_MAPPINGS = {
-    "Fast_Video_ComfyUI": FastImageListToImageBatch
+    "FastImageListToImageBatch": FastImageListToImageBatch
 }
